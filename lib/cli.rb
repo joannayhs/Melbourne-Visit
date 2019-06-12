5c72b20dd579e8a12b1402938527ea8b1146b6d7
Joanna Yuhas <joannayuhas@gmail.com>
2019-06-12 19:43:18 -0400

    created category class

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,37 +3,21 @@ class CLI
   def call
     Scraper.url_parser
     welcome
-    activity_list
+    category_list
   end
 
   def welcome
     puts "Welcome to Melbourne, Australia!"
+    puts "What would you like to do while you're here?"
   end
 
-  def activity_list
-      display_list
-      number = nil
-      while number != "exit"
-      puts "Which activity or event would you like to know more about? (1-22)"
-      puts "To see the list again, type 'list'"
-      puts "To see the list separated into categories, type 'category'"
-      puts "When finished, type 'exit'"
-      number = gets.strip
-      list_number = Attraction.sort_by_name.each_with_index{|attraction, i| i+1}
-        if number.to_i == list_number
-          puts details(number-1)
-        elsif number == "list"
-          display_list
-        elsif number == "exit"
-          goodbye
-        else
-          puts "Please enter a number 1-22:"
-        end
-      end
+  def category_list
+      categories
+      puts "Type a number to see a list of attractions and events in each category:"
   end
 
-  def display_list
-    Attraction.sort_by_name.each_with_index do |attraction, i|
+  def categories
+    Attraction.sort_by_category.each_with_index do |attraction, i|
       puts "#{i+1}. #{attraction}"
     end
   end
@@ -45,7 +29,6 @@ class CLI
       puts "#{selection.category}\n"
       puts "#{selection.description}\n"
       puts "#{selection.location}"
-    end
   end
 
   def goodbye
